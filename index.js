d227b0a5f9cadef0958573f2b1eebe9d172edf6c
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-05-17 18:49:36 -0700

    server running

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ Go code!
 require("dotenv").config();
 const server = require("./server");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`\n* Server Running on http://localhost:${port} *\n`);
+  console.log(`\n* Server listening on http://localhost:${port} *\n`);
 });
