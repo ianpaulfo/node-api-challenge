6bb2938cae4e5a8968421b51444ae90c80174858
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-05-17 19:29:36 -0700

    added CRUD on projects - MVP

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 server.use("/api/actions", actionRouter);
-// server.use("/api/projects", projectRouter);
+server.use("/api/projects", projectRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h1>Node API Sprint - Ian Fragoso</h1>`);
